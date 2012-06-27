eddaaa132fa7785a367e9b0538c686a27a6dd477
Max Persson <max@maxpersson.com>
2012-06-27 17:19:48 +0200

    Add initial app class

--- skal.py
+++ skal.py
@@ -12,3 +12,109 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""
+EXAMPLE APP
+===========
+
+In file myapp:
+
+#!/bin/env python
+
+from skal import Skal
+
+class MyApp(SkalApp):
+    def hello(self):
+        print('hello')
+
+    def yes(self):
+        print('yes')
+
+if __name__ == '__main__':
+    app = MyApp()
+    sys.exit(app.run())
+
+
+USAGE
+=====
+
+> myapp hello
+hello
+
+> myapp yes
+yes
+
+
+CUSTOM ARGUMENTS
+================
+
+#!/bin/env python
+
+from skal import Skal, arguments
+
+class MyApp(Skal):
+    \"\"\"Application description
+    \"\"\"
+
+    __arguments__ = {
+        '-a': {'help': 'Help for a'},
+        '-b': {'help': 'Help for b'}
+    }
+
+    @arguments({
+        '-d': {'help': 'Help for d', 'alt': '--delete', 'action': 'store_true'}
+    })
+    def hello(self):
+        \"\"\"Help for hello
+        \"\"\"
+        if (self.args.delete):
+            print('deleting')
+        print('hello')
+
+    def yes(self):
+        \"\"\"Help for yes
+        \"\"\"
+        print('yes')
+
+if __name__ == '__main__':
+    app = MyApp()
+    sys.exit(app.run())
+
+
+"""
+
+import sys
+from argparse import ArgumentParser
+import inspect
+
+
+class SkalApp(object):
+    def __init__(self):
+        self.__argparser = ArgumentParser(description = self.__doc__)
+        self.__argparser.add_argument(
+                '--version',
+                action='version',
+                version='%(prog)s 2.0')
+        self.__subparser = self.__argparser.add_subparsers(
+                dest = 'command',
+                help = "Operation mode")
+
+        # command = self.__subparser.add_parser('ls', help = "List jobs")
+        # command.set_defaults(cmd = cmd.list_jobs)
+
+
+    def run(self):
+        reserved = [name for name, method in inspect.getmembers(SkalApp, inspect.ismethod)]
+        for name, method in inspect.getmembers(self.__class__, inspect.ismethod):
+            if name not in reserved:
+                method(self)
+                print name
+
+
+class MyApp(SkalApp):
+    def test(self):
+        print('test output')
+
+if __name__ == '__main__':
+    app = MyApp()
+    sys.exit(app.run())
+
