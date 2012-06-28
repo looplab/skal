87c3f85e77a032ca7b86774054007fca3bd6ed10
Max Persson <max@maxpersson.com>
2012-06-28 11:01:12 +0200

    Add test cases
    The test cases are ment to be run with the nose testing suite with the
    command "nosetests". Further testing will be added.

--- test.py
+++ test.py
@@ -12,5 +12,76 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import unittest
+
+import sys
+from nose.tools import raises, with_setup
+
+from skal import SkalApp, command
+
+
+# --- Stderr output helpers ---------------------------------------------------
+
+
+save_stderr = None
+
+
+def disable_stderr():
+    global save_stderr
+    save_stderr = sys.stderr
+    class Devnull(object):
+        def write(self, _): pass
+    sys.stderr = Devnull()
+
+
+def enable_stderr():
+    global save_stderr
+    sys.stderr = save_stderr
+
+
+# --- Skal test class ---------------------------------------------------------
+
+
+class TestApp(SkalApp):
+    @command
+    def first(self):
+        """A command"""
+        print('first')
+
+    def second(self):
+        """Not a command"""
+        print('second')
+
+
+# --- Test cases --------------------------------------------------------------
+
+
+def test_valid_command():
+    args = ['first']
+    TestApp().run(args)
+
+
+@raises(SystemExit)
+@with_setup(disable_stderr, enable_stderr)
+def test_not_a_command():
+    args = ['second']
+    TestApp().run(args)
+
+
+@raises(SystemExit)
+@with_setup(disable_stderr, enable_stderr)
+def test_not_a_method():
+    args = ['third']
+    TestApp().run(args)
+
+
+def test_decorator_command():
+    @command
+    def test():
+        pass
+    assert(hasattr(test, 'skal_meta'))
+
+
+# def test_valid_docstring():
+#     args = ['first']
+#     TestApp().run(args)
 
