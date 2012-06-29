84fbe64d89f848a91f278e0be4368d79419e030d
Max Persson <max@maxpersson.com>
2012-06-29 17:47:21 +0200

    Add output capture to compare console output
    This is done with the StringIO class that saves all the stdout and
    stderr durin the each test. This can then be comared with the
    excpected output to verify the test. Also add a test that the
    correct docstring appears in the usage output.

--- tests.py
+++ tests.py
@@ -14,6 +14,7 @@
 
 
 import sys
+import StringIO
 from nose.tools import raises, with_setup
 
 from skal import SkalApp, command
@@ -22,20 +23,27 @@ from skal import SkalApp, command
 # --- Stderr output helpers ---------------------------------------------------
 
 
-save_stderr = None
+real_stdout = None
+real_stderr = None
+
+captured_stdout = None
+captured_stderr = None
 
 
-def disable_stderr():
-    global save_stderr
-    save_stderr = sys.stderr
-    class Devnull(object):
-        def write(self, _): pass
-    sys.stderr = Devnull()
+def start_capture():
+    global real_stdout, real_stderr
+    global captured_stdout, captured_stderr
+    real_stdout = sys.stdout
+    real_stderr = sys.stderr
+    captured_stdout = StringIO.StringIO()
+    captured_stderr = StringIO.StringIO()
+    sys.stdout = captured_stdout
+    sys.stderr = captured_stderr
 
 
-def enable_stderr():
-    global save_stderr
-    sys.stderr = save_stderr
+def stop_capture():
+    sys.stdout = real_stdout
+    sys.stderr = real_stderr
 
 
 # --- Skal test class ---------------------------------------------------------
@@ -44,31 +52,33 @@ def enable_stderr():
 class TestApp(SkalApp):
     @command
     def first(self):
-        """A command"""
+        """first command"""
         print('first')
 
     def second(self):
-        """Not a command"""
+        """second command"""
         print('second')
 
 
 # --- Test cases --------------------------------------------------------------
 
 
+@with_setup(start_capture, stop_capture)
 def test_valid_command():
     args = ['first']
     TestApp().run(args)
+    assert 'first' in captured_stdout.getvalue(), 'output should be first'
 
 
 @raises(SystemExit)
-@with_setup(disable_stderr, enable_stderr)
+@with_setup(start_capture, stop_capture)
 def test_not_a_command():
     args = ['second']
     TestApp().run(args)
 
 
 @raises(SystemExit)
-@with_setup(disable_stderr, enable_stderr)
+@with_setup(start_capture, stop_capture)
 def test_not_a_method():
     args = ['third']
     TestApp().run(args)
@@ -78,10 +88,15 @@ def test_decorator_command():
     @command
     def test():
         pass
-    assert(hasattr(test, 'skal_meta'))
+    assert hasattr(test, 'skal_meta'), 'function should have Skal metadata'
 
 
-# def test_valid_docstring():
-#     args = ['first']
-#     TestApp().run(args)
-
+@with_setup(start_capture, stop_capture)
+def test_valid_help_string():
+    args = ['-h']
+    try:
+        TestApp().run(args)
+    except SystemExit:
+        pass
+    docstring = TestApp.first.__doc__
+    assert docstring in captured_stdout.getvalue(), ('output should be "%s"' % docstring)
