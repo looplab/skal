0467bf8c3ac165e114a8c0de94285bdc1ddb3319
Max Persson <max@maxpersson.com>
2012-06-29 19:05:36 +0200

    Add tests for global arguments
    A lot of tests for the global arguments feature using the __skal__
    dictionary in the derived class.
    
    There are also some updates to the other tests to make them more
    uniform and added functionality for debug output.

--- tests.py
+++ tests.py
@@ -29,6 +29,8 @@ real_stderr = None
 captured_stdout = None
 captured_stderr = None
 
+debug_output = True
+
 
 def start_capture():
     global real_stdout, real_stderr
@@ -42,18 +44,36 @@ def start_capture():
 
 
 def stop_capture():
+    global captured_stdout, captured_stderr
     sys.stdout = real_stdout
     sys.stderr = real_stderr
+    if debug_output:
+        if captured_stdout.getvalue() != "":
+            print('\nStdout:\n%s' % captured_stdout.getvalue())
+        if captured_stderr.getvalue() != "":
+            print('\nStderr:\n%s' % captured_stderr.getvalue())
+    captured_stdout = None
+    captured_stderr = None
 
 
 # --- Skal test class ---------------------------------------------------------
 
 
 class TestApp(SkalApp):
+
+    __skal__ = {
+        '-b': {'help': 'bool argument', 'action': 'store_true'},
+        ('-s', '--string'): {'help': 'string argument with long name'}
+    }
+
     @command
     def first(self):
         """first command"""
         print('first')
+        if self.args.b:
+            print('b')
+        if self.args.string:
+            print(self.args.string)
 
     def second(self):
         """second command"""
@@ -63,27 +83,7 @@ class TestApp(SkalApp):
 # --- Test cases --------------------------------------------------------------
 
 
-@with_setup(start_capture, stop_capture)
-def test_valid_command():
-    args = ['first']
-    TestApp().run(args)
-    assert 'first' in captured_stdout.getvalue(), (
-            'output should be first')
-
-
-@raises(SystemExit)
-@with_setup(start_capture, stop_capture)
-def test_not_a_command():
-    args = ['second']
-    TestApp().run(args)
-
-
-@raises(SystemExit)
-@with_setup(start_capture, stop_capture)
-def test_not_a_method():
-    args = ['third']
-    TestApp().run(args)
-
+# Decorator tests
 
 def test_decorator_command():
     @command
@@ -93,13 +93,103 @@ def test_decorator_command():
             'function should have Skal metadata')
 
 
+# Command tests
+
 @with_setup(start_capture, stop_capture)
-def test_valid_help_string():
+def test_command_existance():
+    value = 'first'
+    args = [value]
+    TestApp().run(args)
+    assert value in captured_stdout.getvalue(), (
+            'output should contain "%s"' % value)
+
+
+@with_setup(start_capture, stop_capture)
+def test_command_help():
     args = ['-h']
     try:
         TestApp().run(args)
     except SystemExit:
         pass
-    docstring = TestApp.first.__doc__
-    assert docstring in captured_stdout.getvalue(), (
-            'output should be "%s"' % docstring)
+    doc = TestApp.first.__doc__
+    assert doc in captured_stdout.getvalue(), (
+            'help string should be "%s"' % doc)
+
+
+@raises(SystemExit)
+@with_setup(start_capture, stop_capture)
+def test_command_without_decorator():
+    args = ['second']
+    TestApp().run(args)
+
+
+@raises(SystemExit)
+@with_setup(start_capture, stop_capture)
+def test_invalid_command():
+    args = ['third']
+    TestApp().run(args)
+
+
+# Global argument tests
+
+@with_setup(start_capture, stop_capture)
+def test_global_argument_existance():
+    args = ['-h']
+    try:
+        TestApp().run(args)
+    except SystemExit:
+        pass
+    arg = 'b'
+    assert arg in captured_stdout.getvalue(), (
+            'help should contain variable "%s"' % arg)
+
+
+@with_setup(start_capture, stop_capture)
+def test_global_argument_help():
+    args = ['-h']
+    try:
+        TestApp().run(args)
+    except SystemExit:
+        pass
+    arg = 'b'
+    assert arg in captured_stdout.getvalue(), (
+            'help should contain variable "%s"' % arg)
+
+
+@with_setup(start_capture, stop_capture)
+def test_global_argument_value_bool():
+    args = ['-b', 'first']
+    try:
+        TestApp().run(args)
+    except SystemExit:
+        pass
+    value = 'b'
+    assert value in captured_stdout.getvalue(), (
+            'output should contain "%s"' % value)
+
+
+@with_setup(start_capture, stop_capture)
+def test_global_argument_value_string():
+    value = 'test'
+    args = ['--string='+value, 'first']
+    try:
+        TestApp().run(args)
+    except SystemExit:
+        pass
+    assert value in captured_stdout.getvalue(), (
+            'output should contain "%s"' % value)
+
+
+@with_setup(start_capture, stop_capture)
+def test_global_argument_value_bool_and_string():
+    value1 = 'b'
+    value2 = 'test'
+    args = ['-b', '--string='+value2, 'first']
+    try:
+        TestApp().run(args)
+    except SystemExit:
+        pass
+    assert value1 in captured_stdout.getvalue(), (
+            'output should contain "%s"' % value1)
+    assert value2 in captured_stdout.getvalue(), (
+            'output should contain "%s"' % value2)
